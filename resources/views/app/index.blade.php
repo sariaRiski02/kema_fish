ae1ac41111bff893fa7a6c2e35113570c688e6fe
sariaRiski02 <muhammatsaria026@student.unsrat.ac.id>
2024-06-23 17:54:54 +0800

    build: make navbar and the responsive

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="icon" href="{{ asset('/assets/logo/blue.png') }}">
+    @vite(['resources/css/app.css','resources/js/app.js','resources/css/style.css'])
     <title>Document</title>
 </head>
 
--- resources/views/headers/header.blade.php
+++ resources/views/headers/header.blade.php
@@ -1,74 +1,69 @@
-<nav class="bg-white border-gray-200 dark:bg-gray-900 py-5">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
-        <a href="{{ route('home') }}" class="flex items-center space-x-3 rtl:space-x-reverse border">
+<nav class="fixed top-0 right-0 left-0 bg-white  dark:bg-gray-900 py-5">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4 gap-2">
 
-            <span class="self-center text-2xl whitespace-nowrap  dark:text-white font-outfit font-bold">
+        <a href="{{ route('home') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="{{ asset('assets/logo/blue.png') }}" class="h-8" alt="Flowbite Logo" />
+            <span class="self-center text-3xl whitespace-nowrap
+             dark:text-white font-outfit font-bold color-kema-fish" id="logo">
                 KemaFish
             </span>
         </a>
-        <button data-collapse-toggle="navbar-default" type="button"
-            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M1 1h15M1 7h15M1 13h15" />
-            </svg>
-        </button>
 
-
-        <form class="flex items-center max-w-sm mx-auto grow border">
+        <form class="flex items-center flex-grow-3 mx-auto  ">
             <label for="simple-search" class="sr-only">Search</label>
             <div class="relative w-full">
-                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2" />
-                    </svg>
-                </div>
+
                 <input type="text" id="simple-search"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Search branch name..." required />
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 font-outfit"
+                    placeholder="Senar, lure, timah dll.." />
             </div>
-            <button type="submit"
-                class="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                </svg>
-                <span class="sr-only">Search</span>
-            </button>
         </form>
 
+        <div class="flex items-center justify-center gap-5" id="feature">
 
+            {{-- cart --}}
+            <div class="flex items-center justify-center" id="cart">
+                <a href="#">
+                    <img src="{{ asset('/assets/icon/cart.svg') }}" alt="">
+                </a>
+            </div>
 
-        <div class="hidden w-full md:block md:w-auto border" id="navbar-default">
-            <ul
-                class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                    <a href="#"
-                        class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                        aria-current="page">Home</a>
-                </li>
-                <li>
-                    <a href="#"
-                        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-                </li>
-                <li>
-                    <a href="#"
-                        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-                </li>
-                <li>
-                    <a href="#"
-                        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-                </li>
-                <li>
-                    <a href="#"
-                        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-                </li>
-            </ul>
+            <div class="flex items-center justify-center font-thin">
+                <span class="text-xl font-weight-light m-0 p-0">|</span>
+            </div>
+
+            {{-- maps --}}
+            <div class="flex items-center justify-center">
+                <a href="#" id="maps"
+                    class="flex items-center justify-center text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2  text-center gap-2 dark:focus:ring-[#3b5998]/55">
+                    <img src="{{ asset('/assets/icon/pin.svg') }}" alt="">
+                    <span class="location">
+                        Lihat Lokasi
+                    </span>
+                </a>
+            </div>
+
+            {{-- signin --}}
+            <div class="flex items-center justify-center">
+                <a href="#"
+                    class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+                    Masuk
+                </a>
+            </div>
+
+            {{-- signup --}}
+            <div class="flex items-center justify-center">
+                <a href="#"
+                    class=" border bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center text-white">
+                    Daftar
+                </a>
+            </div>
+
         </div>
+
+    </div>
+
+    <div id="phone">
+        @include('headers.phone-menu')
     </div>
 </nav>
