dea60d3c98698c67129999a032b35c1ac646bd7e
sariaRiski02 <muhammatsaria026@student.unsrat.ac.id>
2024-07-24 12:55:03 +0800

    update Readme.md

--- resources/views/components/headers/header.blade.php
+++ resources/views/components/headers/header.blade.php
@@ -14,7 +14,8 @@
             <div class="relative w-full">
 
                 <input type="text" id="simple-search"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 font-outfit"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                     focus:ring-blue-500 focus:border-blue-500 block w-full  p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 font-outfit"
                     placeholder="Senar, lure, timah dll.." />
             </div>
         </form>
